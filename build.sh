ab675fccd3461b8c000b9d31ba7470ec98fa46b7
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-09-13 23:09:06 -0700

    bug fixes in HBase course build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ apt-get update
 apt-get install -y curl
 apt-get install -y hbase sqoop libmysql-java
 
-# Build hwxu/hdp_hive_node
-echo -e "\n*** Building hwux/hdp_hive_zookeeper_node ***\n"
-cd /root/dockerfiles/hdp_hive_zookeeper_node
-docker build -t hwxu/hdp_hive_zookeeper_node .
-echo -e "\n*** Build of hwxu/hdp_hive_zookeeper_node complete! ***\n"
-
 # Build hwxu/hdp_zookeeper_node
 echo -e "\n*** Building hwux/hdp_zookeeper_node ***\n"
 cd /root/dockerfiles/hdp_zookeeper_node
@@ -24,7 +18,14 @@ cd /root/dockerfiles/hdp_hbase_node
 docker build -t hwxu/hdp_hbase_node .
 echo -e "\n*** Build of hwxu/hdp_hbase_node complete! ***\n"
 
+# Build hwxu/hdp_hive_hbase_node
+echo -e "\n*** Building hwux/hdp_hive_hbase_node ***\n"
+cd /root/dockerfiles/hdp_hive_hbase_node
+docker build -t hwxu/hdp_hive_hbase_node .
+echo -e "\n*** Build of hwxu/hdp_hive_hbase_node complete! ***\n"
+
 cp /root/$REPO_DIR/conf/hbase-site.xml /etc/hbase/conf/hbase-site.xml
+echo "alias mysql='mysql -h hiveserver'" >> /etc/profile
 
 remove_untagged_images.sh
 
